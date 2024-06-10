2bb29837f174ff8713572062ad644a616c36f368
unknown <17bhn.ronaldong@gmail.com>
2024-06-10 08:02:43 +0700

    Home work week 6 part 2

--- 06-Streaming-processing/homework/homework.py
+++ 06-Streaming-processing/homework/homework.py
@@ -11,8 +11,8 @@ spark = SparkSession \
     .config('spark.jars.packages', kafka_jar_package) \
     .getOrCreate()
 
-topic_name = 'test-topic'
-topic_name = 'green-trips'
+topic_name = 'green-trips-3'
+# topic_name = 'green-trips'
 server = 'redpanda-1:9092'
 server = 'localhost:9092'
 green_stream = spark \
@@ -34,6 +34,40 @@ def  peek(mini_batch, batch_id):
 # query.start()
 # query.awaitTermination()
 
-query = green_stream.writeStream.foreachBatch(peek).start()
+# query = green_stream.writeStream.foreachBatch(peek).start()
+from pyspark.sql import types
+
+schema = types.StructType() \
+    .add("lpep_pickup_datetime", types.StringType()) \
+    .add("lpep_dropoff_datetime", types.StringType()) \
+    .add("PULocationID", types.IntegerType()) \
+    .add("DOLocationID", types.IntegerType()) \
+    .add("passenger_count", types.DoubleType()) \
+    .add("trip_distance", types.DoubleType()) \
+    .add("tip_amount", types.DoubleType())
+
+from pyspark.sql import functions as F
+
+
+
+green_stream = green_stream \
+            .select(F.from_json(F.col('value').cast('STRING'), schema=schema).alias('data')) \
+            .select("data.*") \
+            
+popular_destinations = green_stream \
+                    .withColumn('timestamp', F.current_timestamp()) \
+                    .groupby('DOLocationID') \
+                    .count() \
+                    .orderBy('count', ascending=False)
+
+write_query = popular_destinations.writeStream \
+        .outputMode('complete') \
+        .trigger(processingTime='10 seconds') \
+        .option('truncate', False) \
+        .format('console') \
+        .start()
+
+write_query.awaitTermination()
+
 import time
 time.sleep(60)
